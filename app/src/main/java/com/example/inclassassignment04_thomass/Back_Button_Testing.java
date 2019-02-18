31b64be9866570e449b253bb9a1a3bc393ae289f
ths2129 <ths2129@users.noreply.github.com>
2019-02-17 22:34:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/inclassassignment04_thomass/Back_Button_Testing.java
@@ -0,0 +1,30 @@
+package com.example.inclassassignment04_thomass;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Back_Button_Testing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back__button__testing);
+
+/*        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+       @Override
+    public boolean onOptionsItemSelected (MenuItem back) {
+           int btn = back.getItemId();
+
+           if (btn == android.R.id.home){
+           this.finish();
+       }
+        return super.onOptionsItemSelected(back);
+    }*/
+}}
---
+++ app/src/main/java/com/example/inclassassignment04_thomass/MainActivity.java
@@ -0,0 +1,236 @@
+package com.example.inclassassignment04_thomass;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity {
+
+    long digit;
+    float fl;
+    TextView display;
+    int num;
+    EditText inPut;
+    TextView outPut;
+    private Button mCopyButton;
+    private Button mDoubleButton;
+    private Button mUpButton;
+    private Button mDownButton;
+    String answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(MainActivity.this, R.string.welcome, Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_main);
+
+        inPut = (EditText) findViewById(R.id.type_something); //EditText ID
+        outPut = (TextView) findViewById(R.id.outcome_view); //TextView ID
+
+        String s1 = inPut.getText().toString();
+
+
+        mCopyButton = (Button) findViewById(R.id.copyText);
+        mCopyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.copyText, Toast.LENGTH_SHORT).show();
+                outPut.setText(inPut.getText());
+                outPut.setText(inPut.getText());
+
+
+            }
+        });
+
+
+        mDoubleButton = (Button) findViewById(R.id.doubleText);
+        mDoubleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.doubleText, Toast.LENGTH_SHORT).show();
+                outPut.setText(inPut.getText().toString());
+                final Button doubleButton = (Button) findViewById(R.id.doubleText);
+                doubleButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        doubleButton.getText().toString();
+                        inPut.setText(outPut.getText());
+
+                    }
+                });
+            }
+                });
+
+
+        mUpButton = (Button) findViewById(R.id.upText);
+        mUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.upText, Toast.LENGTH_SHORT).show();
+                outPut.setText(inPut.getText());
+                final Button upButton = (Button) findViewById(R.id.upText);
+                final int[] size = {20}; //textsize in XML
+
+                upButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        size[0] += 3;
+                        outPut.setTextSize(size[0] + 4);
+                    }
+                });
+
+            }
+        });
+
+
+        mDownButton = (Button) findViewById(R.id.downText);
+        mDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.downText, Toast.LENGTH_SHORT).show();
+                outPut.setText(inPut.getText());
+                final Button downButton = (Button) findViewById(R.id.downText);
+                final int[] size = {12}; //textsize in XML
+
+                downButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        size[0] = size[0] - 1;
+                        outPut.setTextSize(size[0] - 1);
+                    }
+                });
+            }
+        });
+
+
+    }
+/*
+
+
+    public void copyText(View View) { //Prints EditText to the TextView
+
+    }
+
+    public void doubleText(View view) { //Prints EditText to the TextView
+
+
+    }
+
+    public void upText(View V) { //Prints EditText to the TextView
+        inPut = (EditText) findViewById(R.id.type_something); //EditText ID
+        outPut = (TextView) findViewById(R.id.outcome_view); //TextView ID
+        outPut.setText(inPut.getText()); //TextView ID getting the user text in EditText
+
+        //final
+        final Button upButton = (Button) findViewById(R.id.copyText);
+        final float[] size = {12}; //textsize in XML
+
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                size[0] += 1;
+                inPut.setTextSize(size[0] + 4);
+            }
+        });
+    }*/
+}
+
+
+// public void upText (String outCome){
+//    EditText typeSomething = (EditText) findViewById(R.id.type_something);
+//    typeSomething.setText(String.valueOf(savedApp));
+
+
+//    public void copyText (View view){
+//        EditText typeSomething = (EditText) findViewById(R.id.type_something);
+//        TextView outcome = (TextView) findViewById(R.id.outcome_view);
+//        typeSomething.getText().toString();
+//        //TextView.set
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+        if (savedInstanceState != null) {  // If app has never been opened before, than null
+            savedApp = savedInstanceState.getString(savedApp);
+        }
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        //TextView.getText(savedInstanceState.getString(savedApp));
+    }
+
+    // invoked when the activity may be temporarily destroyed, save the instance state here
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // outState.putString(GAME_STATE_KEY, savedApp);
+        //outState.putString(TEXT_VIEW_KEY, textView.getText());
+
+        // call superclass to save any view hierarchy
+        super.onSaveInstanceState(outState);
+
+        // set user interface in the onCreate fragment
+    }
+
+
+
+
+
+
+        public void back(View view){
+        startActivity(new Intent(this, Back_Button_Testing.class));
+        }}
+
+
+        */
